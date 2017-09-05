2a50b5926451b27e066d518c00eb10198e30dea4
sunaliseth <sunaliseth01@gmail.com>
2017-09-05 15:53:49 +0100

    #7,8,9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,39 @@
 class Oystercard
 
 MAX_BALANCE = 90
+MIN_BALANCE = 1
+
 attr_reader :balance
+attr_reader :in_use
 
 def initialize
   @balance = 0
+  @in_use = false
+
 end
 
 def topup(amount)
   fail "Unable to topup; limit is #{MAX_BALANCE}" if amount + balance > MAX_BALANCE
   @balance += amount
 end
+
+  def deduct(fare)
+  @balance -= fare
+  end
+
+  def touch_in
+    fail "Insufficient funds" if @balance < MIN_BALANCE
+    @in_use = true
+    return "You have touched in"
+  end
+
+  def touch_out
+    @in_use = false
+    return "You have touched out"
+  end
+
+  def in_journey?
+    @in_use
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,7 @@ subject(:card){ described_class.new }
   end
 
   describe "#top up" do
-  it "is able to topup" do
+  it "is able to topup and add to balance" do
   expect(card).to respond_to(:topup).with(1).argument
   expect{ card.topup(1) }.to change{ card.balance }.by 1
   end
@@ -16,9 +16,42 @@ subject(:card){ described_class.new }
 
   it "does not exceed topup limit" do
   max_balance = Oystercard::MAX_BALANCE
-  message = "Unable to topup; limit is £90"
+  message = "Unable to topup; limit is #{max_balance}"
   card.topup(max_balance)
   expect { card.topup(1) }.to raise_error message
   end
 
+  describe "#deduct" do
+    it "is able to deduct from balance" do
+    card.topup(10)
+    card.touch_out
+    expect(card).to respond_to(:deduct).with(1).argument
+    expect{ card.deduct(5) }.to change{ card.balance }.by -5
+    end
+  end
+
+  it "is initially not in journey" do
+    expect(card).not_to be_in_journey
+  end
+
+  it "lets you touch_in" do
+    card.topup(10)
+    card.touch_in
+    expect(card).to be_in_journey
+  end
+
+  it "lets you touch_out" do
+    card.topup(10)
+    #card.touch_in
+    card.touch_out
+    expect(card).not_to be_in_journey
+  end
+
+  it "doesn't let you touch_in if no min balance" do
+  min_balance = Oystercard::MIN_BALANCE
+  #card.topup(10)
+  #card.touch_in
+  message = "Insufficient funds"
+  expect { card.touch_in }.to raise_error message
+  end
 end
